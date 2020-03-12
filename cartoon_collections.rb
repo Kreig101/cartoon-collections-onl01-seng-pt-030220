3ad4d95c306e1fb41bd48cf887b5e5a5703d2ba2
Craig Storer <craigstorer@gmail.com>
2020-03-12 00:44:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,5 +14,5 @@ end
 
 def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  snacks.find{|i| i cheese_types.include?(i)}
+  snacks.find{|i| cheese_types.include?(i)}
 end
